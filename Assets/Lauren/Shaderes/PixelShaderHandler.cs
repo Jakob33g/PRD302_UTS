cd798882384825c87754cc998634bc2a1a18988f
rori <126128313+roripoo@users.noreply.github.com>
2025-12-01 13:10:21 +1100

    Enemy anims by Pengyu + Skybox import

---
+++ Assets/Lauren/Shaderes/PixelShaderHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[ExecuteInEditMode]
+
+public class PixelShaderHandler : MonoBehaviour
+{
+    public Material effectMaterial;
+    
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
